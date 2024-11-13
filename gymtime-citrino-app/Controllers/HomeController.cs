5ca9839ef43df5e7493bfab80c0a58c806be795a
Diego Santos <diegom.santos03@gmail.com>
2024-11-13 18:21:07 -0300

    feat: iniciando o projeto, criando apenas os dados iniciais

---
+++ gymtime-citrino-app/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using gymtime_citrino_app.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace gymtime_citrino_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
