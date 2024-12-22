04975e6defc95f41cc3ce209373e7d820bf0b3a6
Diego Santos <diegom.santos03@gmail.com>
2024-12-21 21:52:18 -0300

    chore: desenvolvimento da tela de login, controller e css

---
+++ gymtime-citrino-app/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gymtime_citrino_app.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string Email, string Password, bool RememberMe)
+        {
+            if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
+            {
+                ModelState.AddModelError("", "E-mail ou senha inválidos.");
+                return View();
+            }
+
+            if (Email == "admin" && Password == "password")
+            {
+                if (RememberMe)
+                {
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "E-mail ou senha inválidos.");
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(); 
+        }
+    }
+}
