6c31cc879538c9b438db16abdec9fb6786f99055
Diego Santos <diegom.santos03@gmail.com>
2024-12-21 21:54:15 -0300

    chore: desenvolvimento dos models e ajustes das views

---
+++ gymtime-citrino-app/Models/TimeTableModel.cs
@@ -0,0 +1,8 @@
+﻿namespace gymtime_citrino_app.Models
+{
+    public class TimeTableModel : BaseModel
+    {
+        public DateTime Time { get; set; }
+        public int MaximumCapacity { get; set; }
+    }
+}
---
+++ gymtime-citrino-app/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace gymtime_citrino_app.Models
+{
+    public class UserModel : BaseModel
+    {
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
