e656b15fbf83dcdf1f60e7fdab547222933e5664
Diego Maia Santos <90361640+DiegoMaiaSantos@users.noreply.github.com>
2025-05-19 15:28:19 -0300

    feat: criado entidade e configurado context da aplicacao

---
+++ gymtime-citrino-app.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gymtime_citrino_app.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- gymtime-citrino-app.Infrastructure/Context/GymTimeDbContext.cs
+++ gymtime-citrino-app.Infrastructure/Context/GymTimeDbContext.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using gymtime_citrino_app.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace gymtime_citrino_app.Infrastructure.Context
 {
-    internal class GymTimeDbContext
+    public class GymTimeDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public GymTimeDbContext(DbContextOptions<GymTimeDbContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
